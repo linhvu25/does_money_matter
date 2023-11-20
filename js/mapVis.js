d5b551363cb32fc9eb0c6fcdd395410db9941b80
Micah Williams <micahwilliams@college.harvard.edu>
2023-11-20 10:33:16 -0500

    add tooltip

--- js/mapVis.js
+++ js/mapVis.js
@@ -35,7 +35,7 @@ class MapVis {
     vis.viewpoint = { width: 975, height: 410 };
     vis.zoom = vis.width / vis.viewpoint.width;
 
-    vis.map = vis.svg
+    vis.svg
       .append("g")
       .attr("class", "states")
       .attr("transform", `scale(${vis.zoom} ${vis.zoom})`)
@@ -48,6 +48,8 @@ class MapVis {
       .attr("d", vis.path)
       .attr("fill", "#aaaaaa40");
 
+    vis.states = vis.svg.selectAll(".state");
+
     vis.svg
       .append("g")
       .attr("class", "map-title")
@@ -56,7 +58,13 @@ class MapVis {
       .attr("x", vis.width / 2)
       .attr("text-anchor", "middle")
       .attr("y", vis.margin.top)
-      .text("Senate races in 8 States raised more than $100M in 2018 or 2020");
+      .text("Senate races in 8 States cost more than $100M in 2018 or 2020");
+
+    vis.tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .attr("opacity", 1);
 
     vis.wrangleData();
   }
@@ -68,12 +76,31 @@ class MapVis {
 
   updateVis() {
     let vis = this;
-    vis.svg
-      .selectAll(".state")
-      .attr("fill", (d) =>
-        vis.senateSpending[d.properties.name].total_$ > 100000000
-          ? "#aa4aaa"
-          : "#aaaaaa40"
-      );
+    vis.states.attr("fill", (d) =>
+      vis.senateSpending[d.properties.name].total_$ > 100000000
+        ? "#aa4aaa"
+        : "#aaaaaa40"
+    );
+
+    vis.states.on("mouseover", function (event, d) {
+      //   d3.select(this).attr("fill", "red");
+      //   var stateInfo = vis.stateInfoObject[d.properties.name];
+      vis.tooltip
+        .style("opacity", 1)
+        .style("left", event.pageX + 20 + "px")
+        .style("top", event.pageY + "px").html(`
+         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 5px; padding-bottom: 0px;">
+             <h6>${d.properties.name}</h6>         
+             <p>Year: ${
+               vis.senateSpending[d.properties.name].election_year
+             }<br/>Total contributions: ${d3.format("$,")(vis.senateSpending[d.properties.name].total_$)}</p>          
+         </div>`);
+    });
+    vis.states.on("mouseout", function (event, d) {
+      //   d3.select(this).attr("fill", (d) =>
+      //     vis.color(vis.stateInfoObject[d.properties.name][selectedCategory])
+      //   );
+      vis.tooltip.style("opacity", 0).style("left", 0).style("top", 0).html(``);
+    });
   }
 }
