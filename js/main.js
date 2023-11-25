07dbb1387c889508e57eab42a4ee9d6aa401857f
linhvu25 <linhvu@college.harvard.edu>
2023-11-25 12:30:46 -0500

    PRG: work on tree maps

--- js/main.js
+++ js/main.js
@@ -80,6 +80,66 @@ function initVis(states, senateSpending) {
 d3.csv("data/2020_senate_AZ_candidates.csv").then((data) => {
   myPieChart = new PieChart("pieDivRight", data);
   myTreeMap = new TreeMap('treeMap', data)
+
 });
 
 
+// Read data
+d3.csv('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_hierarchy_1level.csv').then((data) => {
+
+  console.log(data);
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 10, bottom: 10, left: 10},
+      width = 445 - margin.left - margin.right,
+      height = 445 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+  var svg = d3.select("#treeMap")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // stratify the data: reformatting for d3.js
+  var root = d3.stratify()
+      .id(function(d) { return d.name; })   // Name of the entity (column name is name in csv)
+      .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
+      (data);
+  root.sum(function(d) { return +d.value })   // Compute the numeric value for each entity
+
+  // Then d3.treemap computes the position of each element of the hierarchy
+  // The coordinates are added to the root object above
+  d3.treemap()
+      .size([width, height])
+      .padding(4)
+      (root)
+
+  console.log("example leaves", root.leaves())
+  // use this information to add rectangles:
+  svg
+      .selectAll("rect")
+      .data(root.leaves())
+      .enter()
+      .append("rect")
+      .attr('x', function (d) { return d.x0; })
+      .attr('y', function (d) { return d.y0; })
+      .attr('width', function (d) { return d.x1 - d.x0; })
+      .attr('height', function (d) { return d.y1 - d.y0; })
+      .style("stroke", "black")
+      .style("fill", "#69b3a2");
+
+  // and to add the text labels
+  svg
+      .selectAll("text")
+      .data(root.leaves())
+      .enter()
+      .append("text")
+      .attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
+      .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+      .text(function(d){ return d.data.name})
+      .attr("font-size", "15px")
+      .attr("fill", "white")
+})
--- js/treeMap.js
+++ js/treeMap.js
@@ -7,6 +7,8 @@ class TreeMap {
     constructor(_parentElement, _data) {
         this.parentElement = _parentElement;
         this.data = _data;
+        this.displayData = [];
+        this.treeData = [];
 
         // call initVis method
         this.initVis();
@@ -34,45 +36,96 @@ class TreeMap {
     wrangleData(){
         let vis = this;
 
-        let treeData = vis.data.map(row => [row["specific_business"], row["broad_sector"], row["total_$"]]);
-        treeData = treeData.map(item => ({
-            child: item["0"],
-            parent: item["1"],
-            value: item["2"],
+        // subset data to broad_sector and $, rename columns
+        vis.displayData = vis.data.map(row => [row["broad_sector"], row["total_$"]]);
+        vis.displayData = vis.displayData.map(item => ({
+            group: item["0"],
+            value: item["1"],
         }));
-        console.log("tree data", treeData);
 
-        treeData.push([""])
+        // extract contribution amount
+        vis.displayData.forEach((row) => {
+            let money = row.value;
+            row.value = Number(money.replace(/[^0-9\.-]+/g, ""));
+        });
+
+        // aggregate $ by broad_sector
+        const groupedSum = vis.displayData.reduce((accumulator, currentValue) => {
+            const { group, value } = currentValue;
+
+            // Check if the group is already in the accumulator
+            if (!accumulator[group]) {
+                accumulator[group] = 0; // Initialize the sum for the group if not present
+            }
+
+            // Add the value to the sum for the current group
+            accumulator[group] += value;
+
+            return accumulator;
+        }, {});
+
+        // convert to array
+        const myArray = Object.keys(groupedSum).map(key => ({ key, value: groupedSum[key] }));
+        console.log("array", myArray)
+
+        vis.treeData = myArray.map(item => ({
+            name: item.key,
+            parent: 'Origin',
+            value: item.value
+        }));
+
+        console.log("array data", vis.treeData)
+
+        const origin = {name: 'Origin', parent: '', value: ''}
+        vis.treeData.push(origin)
+        console.log("tree data", vis.treeData)
 
         // stratify the data: reformatting for d3.js
         var root = d3.stratify()
-            .id(function(d) { return d.child; })   // Name of the entity (column name is name in csv)
+            .id(function(d) { return d.name; })   // Name of the entity (column name is name in csv)
             .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
-            (treeData);
+            (vis.treeData);
         root.sum(function(d) { return +d.value })   // Compute the numeric value for each entity
 
         // Then d3.treemap computes the position of each element of the hierarchy
         // The coordinates are added to the root object above
         d3.treemap()
-            .size([width, height])
+            .size([vis.width, vis.height])
             .padding(4)
             (root)
 
-        console.log(root.leaves())
-        // Then d3.treemap computes the position of each element of the hierarchy
-        // The coordinates are added to the root object above
-        // d3.treemap()
-        //     .size([vis.width, vis.height])
-        //     .padding(4)
-        //     (root)
-        //
-        // console.log(root.leaves())
-        // use this information to add rectangles:
+        console.log("my leaves", root.leaves())
+
+        vis.svg
+            .selectAll("rect")
+            .data(root.leaves())
+            .enter()
+            .append("rect")
+            .attr('x', function (d) { return d.x0; })
+            .attr('y', function (d) { return d.y0; })
+            .attr('width', function (d) { return d.x1 - d.x0; })
+            .attr('height', function (d) { return d.y1 - d.y0; })
+            .style("stroke", "black")
+            .style("fill", "#69b3a2");
+
+        // and to add the text labels
+        vis.svg
+            .selectAll("text")
+            .data(root.leaves())
+            .enter()
+            .append("text")
+            .attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
+            .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+            .text(function(d){ return d.data.name})
+            .attr("font-size", "15px")
+            .attr("fill", "white")
 
         vis.updateVis();
     }
 
     updateVis(){
+        let vis = this;
+        // use this information to add rectangles:
 
     }
 }
